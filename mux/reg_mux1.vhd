1af88ee10dd08e2bbe275691ee794885f204d4d8
James Wong <james.klo23@gmail.com>
2018-03-30 15:22:33 -0700

    Add mux 1 logic

--- mux/reg_mux1.vhd
+++ mux/reg_mux1.vhd
@@ -1,22 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    13:18:22 03/30/2018 
--- Design Name: 
--- Module Name:    reg_mux1 - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -30,11 +11,32 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity reg_mux1 is
+	Port(
+		-- Inputs
+		data_select : IN std_logic_vector(2 downto 0);
+		pc_val : IN std_logic_vector(6 downto 0);
+		data_direct : IN std_logic_vector(15 downto 0);
+		data_reg : IN std_logic_vector(15 downto 0);
+		data_exe : IN std_logic_vector(15 downto 0);
+		data_mem : IN std_logic_vector(15 downto 0);
+		data_wb : IN std_logic_vector(15 downto 0);
+		
+		-- Outputs
+		data_out : OUT std_logic_vector(15 downto 0)
+	);
 end reg_mux1;
 
 architecture Behavioral of reg_mux1 is
 
 begin
+
+	data_out <=
+		("000000000" & pc_val) when data_select = "001" else
+		data_direct when data_select = "010" else
+		data_exe when data_select = "101" else
+		data_mem when data_select = "110" else
+		data_wb when data_select = "111" else
+		data_reg;
 
 
 end Behavioral;
