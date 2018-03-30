1642e791b15521828d5cfec10a2bd20e1ef48643
James Wong <james.klo23@gmail.com>
2018-03-30 16:15:56 -0700

    Add mux 2 logic

--- mux/reg_mux2.vhd
+++ mux/reg_mux2.vhd
@@ -1,22 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    13:21:23 03/30/2018 
--- Design Name: 
--- Module Name:    reg_mux2 - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
-----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -30,11 +11,35 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity reg_mux2 is
+	Port(
+		-- Inputs
+		data_select : IN std_logic_vector(2 downto 0);
+		data_displ : IN std_logic_vector(8 downto 0);
+		data_imm : IN std_logic_vector(15 downto 0);
+		data_reg : IN std_logic_vector(15 downto 0);
+		data_exe : IN std_logic_vector(15 downto 0);
+		data_mem : IN std_logic_vector(15 downto 0);
+		data_wb : IN std_logic_vector(15 downto 0);
+		
+		-- Outputs
+		data_out : OUT std_logic_vector(15 downto 0)
+	);
 end reg_mux2;
 
 architecture Behavioral of reg_mux2 is
 
 begin
+
+	data_out <=
+		data_imm when data_select = "001" else
+		-- 2 * displacement
+		"000000" & data_displ & "0" when ((data_select = "010") and (data_displ(8) = '0')) else
+		-- 2 * displacement (negative)
+		"111111" & data_displ & "0" when ((data_select = "010") and (data_displ(8) = '1')) else
+		data_exe when data_select = "101" else
+		data_mem when data_select = "110" else
+		data_wb when data_select = "111" else
+		data_reg;
 
 
 end Behavioral;
