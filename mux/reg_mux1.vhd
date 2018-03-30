0bf9477b5d9b5cbf7c7dfaba828e31fc18e5a4d0
James Wong <james.klo23@gmail.com>
2018-03-30 13:22:19 -0700

    Created 2 mux before id/exe latch

---
+++ mux/reg_mux1.vhd
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    13:18:22 03/30/2018 
+-- Design Name: 
+-- Module Name:    reg_mux1 - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity reg_mux1 is
+end reg_mux1;
+
+architecture Behavioral of reg_mux1 is
+
+begin
+
+
+end Behavioral;
+
