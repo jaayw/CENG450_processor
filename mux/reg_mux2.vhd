4b25ba41bf02aa68e80190060b10b89b4e7c7c6f
James Wong <james.klo23@gmail.com>
2018-04-01 14:07:34 -0700

    Fixed size mismatch for loadimm

--- mux/reg_mux2.vhd
+++ mux/reg_mux2.vhd
@@ -15,7 +15,7 @@ entity reg_mux2 is
 		-- Inputs
 		data_select : IN std_logic_vector(2 downto 0);
 		data_displ : IN std_logic_vector(8 downto 0);
-		data_imm : IN std_logic_vector(15 downto 0);
+		data_imm : IN std_logic_vector(7 downto 0);
 		data_reg : IN std_logic_vector(15 downto 0);
 		data_exe : IN std_logic_vector(15 downto 0);
 		data_mem : IN std_logic_vector(15 downto 0);
@@ -31,7 +31,7 @@ architecture Behavioral of reg_mux2 is
 begin
 
 	data_out <=
-		data_imm when data_select = "001" else
+		("00000000" & data_imm) when data_select = "001" else
 		-- 2 * displacement
 		"000000" & data_displ & "0" when ((data_select = "010") and (data_displ(8) = '0')) else
 		-- 2 * displacement (negative)
@@ -40,7 +40,6 @@ begin
 		data_mem when data_select = "110" else
 		data_wb when data_select = "111" else
 		data_reg;
-
 
 end Behavioral;
 
